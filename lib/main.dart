c4f78400a6def64ad46cc26bf027463ddd337998
dave-lence <davidfnwudo@gmail.com>
2024-06-02 22:14:38 -0400

    fixing app check error

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -16,6 +17,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseAppCheck.instance.activate(androidProvider:AndroidProvider.playIntegrity);
   runApp(const ProviderScope(child: MyApp()));
 }
 
